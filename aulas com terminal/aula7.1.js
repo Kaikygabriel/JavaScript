431aa93bedb5191fbdb6f3b468f9c70cf1b26966
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2024-06-02 11:12:18 -0300

    atualização do site

--- aulas com terminal/aula7.1.js
+++ aulas com terminal/aula7.1.js
@@ -1,6 +1,6 @@
 var pais = 'Brasil'
 
-if (pais == 'Brasil'){
+if (pais == 'Brasil' || pais == 'brasil'){
     console.log('Voce é brasileiro')
 } else {
     console.log('Voce e estrangeiro')
--- aulas com terminal/aula8.1.js
+++ aulas com terminal/aula8.1.js
@@ -1,11 +1,15 @@
 var agora = new Date
 var horas = agora.getHours()
+
 // para poder pegar a hora em tempo real
+
 console.log(`Agora são exatamente ${horas} horas`)
 if (horas <= 12 && horas >= 6){
     console.log('Bom dia')
-} else if(horas <= 18 && horas == 18){
+} else if(horas <= 18 && horas >= 13){
     console.log('Boa tarde')
-} else {
-    console.log('Boa noite ou Boa madrugada')
+} else if (horas >= 19 && horas <= 23) {
+    console.log('Boa noite')
+} else{
+    console.log('Boa madrugada')
 }
